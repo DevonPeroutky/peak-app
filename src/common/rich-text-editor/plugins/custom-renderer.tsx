75143776f448b1b11d399c3818bf8c574c1a7aa6
DevonPeroutky <devonperoutky@gmail.com>
2020-11-13 20:33:08 -0800

    Remove file

--- src/common/rich-text-editor/plugins/custom-renderer.tsx
+++
@@ -1,8 +0,0 @@
-import {RenderElementProps} from "slate-react";
-import React from "react";
-import {JournalEntryBody, JournalEntryHeader} from "./journal-entry-plugin/journal-entry/JournalEntry";
-import {JOURNAL_ENTRY, JOURNAL_ENTRY_HEADER} from "../types";
-import "./completed-plugin/completed.scss"
-
-
-// @ts-ignore
