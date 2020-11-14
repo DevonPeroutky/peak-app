058ff7eb957a2dd193d519259ef42d7b5c1fdcfd
DevonPeroutky <devonperoutky@gmail.com>
2020-11-13 20:20:40 -0800

    Remove unnecessary customRenderElement

--- src/common/rich-text-editor/editors/journal/constants.ts
+++ src/common/rich-text-editor/editors/journal/constants.ts
@@ -11,6 +11,7 @@ import {withEditableJournalEntry} from "../../plugins/journal-entry-plugin/withE
 import {JOURNAL_ENTRY_HEADER} from "../../types";
 import {JournalEntry} from "../../../../redux/wikiPageSlice";
 import {getCurrentFormattedDate} from "../../../../utils/time";
+import {JournalEntryPlugin} from "../../plugins/journal-entry-plugin/JournalEntryPlugin";
 
 export const JOURNAL_PAGE_ID = "journal"
 export const EMPTY_JOURNAL_STATE: JournalEntry[] = [{
@@ -20,7 +21,7 @@ export const EMPTY_JOURNAL_STATE: JournalEntry[] = [{
 
 // Because we have JournalEntry --> Node[] per JournalEntry, the editor 'depth' is 2 deep instead of the default of 1.
 const nodeLevel: number = 2
-export const journalPlugins: SlatePlugin[] = setEditorPlugins(nodeLevel)
+export const journalPlugins: SlatePlugin[] = setEditorPlugins(nodeLevel, [JournalEntryPlugin()])
 export const journalNormalizers = setEditorNormalizers(nodeLevel, [
     withEditableJournalEntry,
     withInlineVoid({ plugins: journalPlugins, voidTypes: [ELEMENT_CODE_BLOCK, JOURNAL_ENTRY_HEADER] })
--- src/common/rich-text-editor/plugins/custom-renderer.tsx
+++ src/common/rich-text-editor/plugins/custom-renderer.tsx
@@ -1,21 +1,8 @@
 import {RenderElementProps} from "slate-react";
-import PeakCodeEditor from "./peak-code-plugin/code-editor/PeakCodeEditor";
 import React from "react";
-import {ELEMENT_CODE_BLOCK, getRenderElement} from "@udecode/slate-plugins";
 import {JournalEntryBody, JournalEntryHeader} from "./journal-entry-plugin/journal-entry/JournalEntry";
 import {JOURNAL_ENTRY, JOURNAL_ENTRY_HEADER} from "../types";
 import "./completed-plugin/completed.scss"
 
 
 // @ts-ignore
-export const renderCustomPeakElement = (options: any ) => (props: RenderElementProps) => {
-    const { attributes, children, element } = props
-    switch (element.type) {
-        case JOURNAL_ENTRY:
-            return <JournalEntryBody entry_date={props.element.entry_date as string} {...props}/>
-        case JOURNAL_ENTRY_HEADER:
-            return <JournalEntryHeader entry_date={props.element.entry_date as string} {...props}/>
-        default:
-            break;
-    }
-};
