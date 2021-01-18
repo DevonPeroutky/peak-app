69f83747df8717730c2f0c6b84727355db9d5fb8
DevonPeroutky <devonperoutky@gmail.com>
2021-01-17 21:23:46 -0800

    Add comment explaining content inject choices

--- src/chrome-extension/utils/contentUtils.ts
+++ src/chrome-extension/utils/contentUtils.ts
@@ -4,6 +4,12 @@ export const idempotentlyInjectContentScript = (tabId: number, loadTagsAndOpenDr
     chrome.tabs.sendMessage(tabId, { message_type: MessageType.Ping }, responseMessage => {
         // Content Script did respond so it must not be there
         if (!responseMessage) {
+
+            /**
+             * We have to inject lazily vs. declaratively in the manifest, because the css has conflicts with varies website
+             * and this way we minimize the potential conflicts. We also need to inject the content script, as opposed
+             * to injectCSS, because injecting stylesheets with injectCSS will not respect !important.
+             */
             chrome.tabs.executeScript({ file: 'content.js'}, loadTagsAndOpenDrawer);
         } else {
             console.log(`Content script already there. Stand down.`)
