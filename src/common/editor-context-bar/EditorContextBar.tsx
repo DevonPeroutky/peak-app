ce95dd95762be00a8feef0a0e2331fc4820efcb8
DevonPeroutky <devonperoutky@gmail.com>
2021-02-06 16:51:59 -0800

    Found one additional place to migrate to page agnostic editor state

--- src/common/editor-context-bar/EditorContextBar.tsx
+++ src/common/editor-context-bar/EditorContextBar.tsx
@@ -3,6 +3,7 @@ import {HelpModal} from "../modals/help-modal/HelpModal";
 import "./editor-context-bar.scss"
 import {useCurrentWikiPage} from "../../utils/hooks";
 import {CheckOutlined} from "@ant-design/icons/lib";
+import {useActiveEditorState} from "../../redux/slices/activeEditor/activeEditorSlice";
 
 
 export const EditorContextBar = (props: {}) => {
@@ -15,13 +16,13 @@ export const EditorContextBar = (props: {}) => {
 }
 
 const PageEditingContext = (props: {}) => {
-    const currentPage = useCurrentWikiPage()
+    const editorState = useActiveEditorState()
 
-    if (currentPage && currentPage.isSaving) {
+    if (editorState && editorState.isSaving) {
         return (
             <span className={"editing-status"}>Saving...</span>
         )
-    } else if (currentPage && !currentPage.isSaving) {
+    } else if (editorState && !editorState.isSaving) {
         return (
             <span className={"editing-status animated fadeOut saved"}>Saved <CheckOutlined className={"saved-check"}/></span>
         )
