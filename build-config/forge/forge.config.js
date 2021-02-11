94f0e8721d89d5a69f4252d43288b875e562fa80
DevonPeroutky <devonperoutky@gmail.com>
2021-02-10 22:49:02 -0800

    code_sign and notarize the electron app

--- build-config/forge/forge.config.js
+++ build-config/forge/forge.config.js
@@ -1,3 +1,6 @@
+const { notarize } = require('electron-notarize');
+const path = require('path');
+
 module.exports = {
     makers: [
         {
@@ -19,11 +22,44 @@ module.exports = {
             config: {},
         },
     ],
-    "packagerConfig": {
-        protocols: [{"name": "Peak", "schemes": ["peak-dev-app", "peak-app"]}],
-        name: "Peak",
-        executableName: "Peak",
-        "icon": "./src/assets/logos/electron-icons/mac/icon.icns"
+    packagerConfig: {
+        asar: true,
+        "protocols": [{"name": "Peak", "schemes": ["peak-dev-app", "peak-app"]}],
+        "name": "Peak",
+        "executableName": "Peak",
+        "dmg": {
+            "sign": false,
+        },
+        "osxSign": {
+            "identity": process.env.APPLE_DEVELOPER_IDENTITY,
+            "hardenedRuntime": true,
+            "gatekeeper-assess": false,
+            "entitlements": "./build-dependencies/resources/entitlements.mac.plist",
+            "entitlementsInherits": "./build-dependencies/resources/entitlements.mac.plist"
+        },
+        "osxNotarize": {
+            "appleId": process.env.APPLE_ID,
+            "appleIdPassword": process.env.APPLE_ID_PASSWORD
+        },
+        "icon": "./src/assets/logos/electron-icons/mac/icon.icns",
+        packageManager: 'yarn'
+    },
+    "hooks": {
+        "postPackage": async function (params) {
+            console.log('postPackage hook triggered');
+
+            if (process.platform !== 'darwin') {
+                console.log('Skipping notarization - not building for Mac');
+                return;
+            }
+
+            await notarize({
+                // appBundleId: "com.massive.peak",
+                appPath: path.join("out", "Peak-darwin-x64", "Peak.app"),
+                appleId: process.env.APPLE_ID,
+                appleIdPassword: process.env.APPLE_ID_PASSWORD
+            });
+        }
     },
     "plugins": [
         [
