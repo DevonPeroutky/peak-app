c5e75f16bad7d03cb92ee83aeba9258f3b8b477a
DevonPeroutky <devonperoutky@gmail.com>
2021-02-28 23:33:38 -0800

    Add production and publish chrome extension build scripts

--- scripts/chrome-extension/publish.sh
+++ scripts/chrome-extension/publish.sh
@@ -2,12 +2,11 @@
 #parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 #cd "$parent_path"
 
-# Build Production Manifest
-echo "Generating the production manifest.json..."
+echo "Generating the production manifest.json, WITHOUT THE key..."
 REACT_APP_BACKEND_SERVER_ADDRESS=peak-backend.onrender.com
 REACT_APP_APP_SERVER_ADDRESS=peak-app-server.onrender.com
 REACT_APP_ENV=prod
-cp ./scripts/chrome-extension/base_manifest.json public/manifest.json
+cat ./scripts/chrome-extension/base_manifest.json | jq 'del(.key)' > 'public/manifest.json'
 
-echo "Building the production chrome-extension..."
+echo "Building the production chrome-extension that can be submitted on the App Store"
 webpack --config build-config/webpack/webpack.chrome-extension.config.js --mode=production
