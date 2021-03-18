92e6e74ef1b703fea758a4700e588d264f51733f
DevonPeroutky <devonperoutky@gmail.com>
2021-03-17 17:01:13 -0700

    [WIP Checkpoint]

--- src/chrome-extension/content-script/components/save-page-message/SavePageMessage.tsx
+++ src/chrome-extension/content-script/components/save-page-message/SavePageMessage.tsx
@@ -1,5 +1,7 @@
-import {notification} from "antd";
+import * as React from "react";
+import {notification, Spin} from "antd";
 import "./save-page-message.scss";
+import {SavePageHeaderContent, SavingLoader} from "./components/save-page-header-content/SavePageHeaderContent";
 
 notification.config({
     placement: 'topRight',
@@ -24,12 +26,11 @@ export const openEditingNotification = () => {
 
 export const openSavingNotification = () => {
     notification.open({
-        message: 'Saving...',
+        message: <SavePageHeaderContent saving={true}/>,
         className: 'saved-page-message',
         duration: 0,
         key: "saved-page-message",
-        description:
-            'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+        description: <SavingLoader/>,
         onClick: () => {
             console.log('Notification Clicked!');
         },
@@ -37,19 +38,12 @@ export const openSavingNotification = () => {
 };
 
 export const openSavedNotification = () => {
-    notification.config({
-        placement: 'topRight',
-        top: 0,
-        duration: 0,
-        rtl: false,
-    });
     notification.open({
-        message: 'Saved!!!',
-        className: 'saved-page-message drawer-mode',
+        message: <SavePageHeaderContent saving={false}/>,
+        className: 'saved-page-message',
         duration: 0,
         key: "saved-page-message",
-        description:
-            'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+        description: <SavingLoader/>,
         onClick: () => {
             console.log('Notification Clicked!');
         },
--- src/chrome-extension/pop-up/component/Popup.tsx
+++ src/chrome-extension/pop-up/component/Popup.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import "./popup.scss"
 import {PeakLogo} from "../../../common/logo/PeakLogo";
 import {CheckOutlined, BookOutlined, BookTwoTone} from "@ant-design/icons/lib";
-import WebappGoogleLogin from "../../../common/login/signin-button/webapp-google-signin/WebappGoogleLogin";
 
 export const Popup = (props) => {
     return (
