c7e0e404294ea22aac0946e535399e2e7bc0b1a0
DevonPeroutky <devonperoutky@gmail.com>
2021-03-20 18:06:08 -0700

    Reset duration

--- src/chrome-extension/content-script/components/save-page-message/SavePageMessage.tsx
+++ src/chrome-extension/content-script/components/save-page-message/SavePageMessage.tsx
@@ -77,7 +77,7 @@ export const openMessage = (props: SavedPageProps) => {
         message: <SavePageHeaderContent saving={saving} editing={editingState} sendDeletePageMessage={deletePage} goBack={goBack}/>,
         className: cn('peak-saved-page-message', (editingState === EDITING_STATE.Editing) ? "drawer-mode" : ""),
         key: NOTIFICATION_KEY,
-        duration: 0,
+        duration: duration,
         description: <SavePageContent editingState={editingState} {...props}/>,
         closeIcon: <NullButton/>,
         onClick: () => console.log(`CLICKED`),
