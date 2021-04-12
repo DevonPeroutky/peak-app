81372743d2e82f596cfcc8f8b9e4b66879f72b74
DevonPeroutky <devonperoutky@gmail.com>
2021-04-11 19:18:40 -0700

    [WIP] Little bit refractor

--- src/common/rich-text-editor/plugins/peak-media-embed-plugin/components/MediaEmbedStub.tsx
+++ src/common/rich-text-editor/plugins/peak-media-embed-plugin/components/MediaEmbedStub.tsx
@@ -3,26 +3,15 @@ import cn from "classnames";
 import {ClassName, RootStyleSet, StyledElementProps} from "@udecode/slate-plugins";
 import {styled} from "@uifabric/utilities";
 import "./media-embed-stub.scss"
-import {
-    ELEMENT_IMAGE_EMBED,
-    ELEMENT_MEDIA_EMBED,
-    ELEMENT_TWITTER_EMBED,
-    ELEMENT_YOUTUBE_EMBED,
-    PEAK_MEDIA_EMBED
-} from "../types";
-import {
-    PEAK_IMAGE_EMBED_STUB,
-    PEAK_MEDIA_EMBED_STUB,
-    PEAK_TWITTER_EMBED_STUB,
-    PEAK_YOUTUBE_EMBED_STUB, PeakMediaEmbedControl
-} from "../constants";
+import { PeakMediaEmbedControl } from "../constants";
 import {Input, message, Modal} from "antd";
+import {mapEmbeddedTypeToControlObject} from "../utils";
 
 const MediaEmbedStub = ({attributes, children, nodeProps, ...props}: StyledElementProps) => {
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [url, setUrl] = useState<string | null>(null)
 
-    const embedControl: PeakMediaEmbedControl = renderStub(props.element.embed_type)
+    const embedControl: PeakMediaEmbedControl = mapEmbeddedTypeToControlObject(props.element.embed_type)
 
     const embedMedia = () => {
         console.log(`Submitting with `, url)
@@ -61,34 +50,11 @@ const MediaEmbedStub = ({attributes, children, nodeProps, ...props}: StyledEleme
                 okText={"Embed"}
                 onOk={() => embedMedia()}
                 onCancel={() => resetContent()}>
-                <MediaEmbedInput embedControl={embedControl} url={url} setUrl={setUrl} submit={embedMedia}/>
+                <Input placeholder={embedControl.inputPlaceholder} value={url} onChange={e => setUrl(e.target.value)} onPressEnter={embedMedia}/>
             </Modal>
         </>
     );
 };
-
-const renderStub = (embed_type: PEAK_MEDIA_EMBED): PeakMediaEmbedControl => {
-    switch (embed_type) {
-        case ELEMENT_IMAGE_EMBED:
-            return PEAK_IMAGE_EMBED_STUB
-        case ELEMENT_YOUTUBE_EMBED:
-            return PEAK_YOUTUBE_EMBED_STUB
-        case ELEMENT_TWITTER_EMBED:
-            return PEAK_TWITTER_EMBED_STUB
-        case ELEMENT_MEDIA_EMBED:
-            return PEAK_MEDIA_EMBED_STUB
-    }
-}
-
-
-const MediaEmbedInput = (props: {embedControl: PeakMediaEmbedControl, url: string | undefined, setUrl, submit}) => {
-    const { embedControl, url, setUrl, submit } = props
-    return (
-        <div className={"peak-media-embed-input-container"}>
-            <Input placeholder={embedControl.inputPlaceholder} value={url} onChange={e => setUrl(e.target.value)} onPressEnter={submit}/>
-        </div>
-    )
-}
 
 export const PeakMediaStubElement = styled<
     StyledElementProps,
--- src/common/rich-text-editor/plugins/peak-media-embed-plugin/utils.ts
+++ src/common/rich-text-editor/plugins/peak-media-embed-plugin/utils.ts
@@ -1,7 +1,19 @@
 import { SPEditor } from "@udecode/slate-plugins";
 import { Transforms } from "slate";
-import {ELEMENT_EMBED_STUB, PEAK_MEDIA_EMBED} from "./types";
+import {
+    ELEMENT_EMBED_STUB,
+    ELEMENT_IMAGE_EMBED, ELEMENT_MEDIA_EMBED,
+    ELEMENT_TWITTER_EMBED,
+    ELEMENT_YOUTUBE_EMBED,
+    PEAK_MEDIA_EMBED
+} from "./types";
 import {EMPTY_PARAGRAPH_NODE} from "../../editors/constants";
+import {
+    PEAK_IMAGE_EMBED_STUB, PEAK_MEDIA_EMBED_STUB,
+    PEAK_TWITTER_EMBED_STUB,
+    PEAK_YOUTUBE_EMBED_STUB,
+    PeakMediaEmbedControl
+} from "./constants";
 
 export const insertMediaEmbedStub = (editor: SPEditor, mediaEmbedType: PEAK_MEDIA_EMBED) => {
     const nodeId = Date.now()
@@ -19,4 +31,21 @@ export const insertMediaEmbedStub = (editor: SPEditor, mediaEmbedType: PEAK_MEDI
         mediaEmbedNode,
         EMPTY_PARAGRAPH_NODE()
     ]);
+}
+
+export const mapEmbeddedTypeToControlObject = (embed_type: PEAK_MEDIA_EMBED): PeakMediaEmbedControl => {
+    switch (embed_type) {
+        case ELEMENT_IMAGE_EMBED:
+            return PEAK_IMAGE_EMBED_STUB
+        case ELEMENT_YOUTUBE_EMBED:
+            return PEAK_YOUTUBE_EMBED_STUB
+        case ELEMENT_TWITTER_EMBED:
+            return PEAK_TWITTER_EMBED_STUB
+        case ELEMENT_MEDIA_EMBED:
+            return PEAK_MEDIA_EMBED_STUB
+    }
+}
+
+export const insertMediaEmbed = (editor: SPEditor, mediaEmbedType: PEAK_MEDIA_EMBED) => {
+
 }
