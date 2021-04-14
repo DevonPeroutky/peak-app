8e642f840a9bdf3de81e5206300380b611b3a263
DevonPeroutky <devonperoutky@gmail.com>
2021-04-13 17:46:09 -0700

    Rich-link preview styling is done

--- src/common/rich-text-editor/plugins/peak-media-embed-plugin/components/embedded_content/EmbeddedContent.tsx
+++ src/common/rich-text-editor/plugins/peak-media-embed-plugin/components/embedded_content/EmbeddedContent.tsx
@@ -10,22 +10,24 @@ export const RichLinkEmbed = ({attributes, children, nodeProps, ...props}: Style
     console.log(`Rich Link Preview! `, props)
     const { title, url, cover_image_url, fav_icon_url, description } = props.element
     return (
-        <>
-            <div className={"voidable-slate-element editor-rich-link-preview"} {...attributes} contentEditable={false}>
-                <div className={"left-column"}>
-                    {(title) ? <div className={"title"}>{title}</div> : null}
-                    {(description) ? <div className={"description"}>{description}</div> : null}
-                    <div className={"footer"}>
-                        <ImageLoader url={fav_icon_url} fallbackElement={null} className={"fav-icon"}/>
-                        <div className={"hostname"}>{deriveHostname(url)}</div>
+        <div contentEditable={false} className={"voidable-slate-element"}>
+            <a target={"_blank"} href={url} className={"rich-link-preview-container"}>
+                <div className={"editor-rich-link-preview"} {...attributes} >
+                    <div className={"left-column"}>
+                        {(title) ? <div className={"title"}>{title}</div> : null}
+                        {(description) ? <div className={"description"}>{description}</div> : null}
+                        <div className={"footer"}>
+                            <ImageLoader url={fav_icon_url} fallbackElement={null} className={"fav-icon"}/>
+                            <div className={"hostname"}>{deriveHostname(url)}</div>
+                        </div>
+                    </div>
+                    <div className={"right-column"}>
+                        <ImageLoader url={cover_image_url} fallbackElement={null} className={"cover-image"}/>
                     </div>
                 </div>
-                <div className={"right-column"}>
-                    <ImageLoader url={cover_image_url} fallbackElement={null} className={"cover-image"}/>
-                </div>
-            </div>
+            </a>
             {children}
-        </>
+        </div>
     )
 }
 
