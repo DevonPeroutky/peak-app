f506ce0638d85c38f59d205da1f00b87a0ac3ba3
DevonPeroutky <devonperoutky@gmail.com>
2021-04-28 08:04:39 -0700

    Node Select menu open by default

--- src/common/rich-text-editor/utils/node-content-select/useNodeContentSelect.tsx
+++ src/common/rich-text-editor/utils/node-content-select/useNodeContentSelect.tsx
@@ -1,5 +1,5 @@
 import {useCallback, useEffect, useMemo, useState} from 'react';
-import { Editor, Point, Range, Transforms } from 'slate';
+import {Editor, Node, Point, Range, Transforms} from 'slate';
 import {
     ELEMENT_LIC,
     ELEMENT_PARAGRAPH,
@@ -208,12 +208,26 @@ export const useNodeContentSelect = (
                     trigger,
                 });
                 const [currNode, currPath] = Editor.above(editor)
+                const text: string = Node.string(currNode)
 
                 // Restrict NodeSelectMenu to paragraph nodes at (exclusively the top-leve) for sanity reasons
-                const atTopLevel: boolean = isAtTopLevelOfEditor(editor.selection, editorLevel)
                 const currentlyInParagraphNode: boolean = currNode.type === ELEMENT_PARAGRAPH || currNode.type === ELEMENT_LIC
 
-                if (atEnd && beforeMatch && currentlyInParagraphNode && atTopLevel) {
+                // HOW TO ESCAPE
+                let re = /^\/[a-zA-Z]+/
+                const res = re.exec(text)
+                console.log(`RES `, res)
+                // const atBeginning: boolean =
+
+                // Default Open
+                if (atEnd && text === trigger) {
+                    setTargetRange(editor.selection);
+                    setSearch("");
+                    setValueIndex(0);
+                    return;
+                }
+
+                if (atEnd && beforeMatch && currentlyInParagraphNode) {
                 // if (atEnd && beforeMatch) {
                     setTargetRange(range as Range);
                     const [, word] = beforeMatch;
