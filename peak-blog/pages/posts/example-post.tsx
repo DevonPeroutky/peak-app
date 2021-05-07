64b35e2e90a499a8c71a3524fd27d9b8a3f7bf51
DevonPeroutky <devonperoutky@gmail.com>
2021-05-07 15:49:21 -0400

    Add typescript support

---
+++ peak-blog/pages/posts/example-post.tsx
@@ -0,0 +1,9 @@
+import styles from "../../styles/Home.module.css";
+
+export default function ExamplePost() {
+    return (
+        <div className={styles.container}>
+            <h1>Big Things</h1>
+        </div>
+    )
+}
