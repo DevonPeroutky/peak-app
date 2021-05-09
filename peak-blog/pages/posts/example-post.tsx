05499d27bbc04e93f6a1380455d69b0b091d66c3
DevonPeroutky <devonperoutky@gmail.com>
2021-05-09 17:46:42 -0400

    Skeleton out directory structure

--- peak-blog/pages/posts/example-post.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "../../styles/Home.module.css";
-
-export default function ExamplePost() {
-    return (
-        <div className={styles.container}>
-            <h1>Big Things</h1>
-        </div>
-    )
-}
---
+++ peak-blog/src/pages/posts/example-post.tsx
@@ -0,0 +1,9 @@
+import styles from "../../../styles/Home.module.css";
+
+export default function ExamplePost() {
+    return (
+        <div className={"flex flex-grow "}>
+            <h1 className={"text-4xl"}>Big Things</h1>
+        </div>
+    )
+}
