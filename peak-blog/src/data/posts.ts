b40759b3c3d24c3951e6a818ea46f5942145a2ca
DevonPeroutky <devonperoutky@gmail.com>
2021-05-11 21:22:30 -0400

    Add types

---
+++ peak-blog/src/data/posts.ts
@@ -0,0 +1,10 @@
+import {BlogPost, HydratedSubdomain} from "../types";
+
+export function fetch_blog_posts(subdomain: string): BlogPost[] {
+    return []
+}
+
+
+export function fetch_subdomain(subDomain: string): HydratedSubdomain {
+    return undefined
+}
