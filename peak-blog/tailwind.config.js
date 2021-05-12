a3c8e80aab05b8bfef68a97fb477ddfc64dfa5f9
DevonPeroutky <devonperoutky@gmail.com>
2021-05-12 15:28:48 -0400

    Add Inter as the font

--- peak-blog/tailwind.config.js
+++ peak-blog/tailwind.config.js
@@ -1,10 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Helvetica Neue'],
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        // sans: ['Helvetica Neue'],
       },
     },
   },
