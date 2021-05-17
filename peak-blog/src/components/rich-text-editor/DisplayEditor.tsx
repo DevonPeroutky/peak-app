7a2ced8191d7ff11beb4c69201bfc91984172284
DevonPeroutky <devonperoutky@gmail.com>
2021-05-17 00:17:43 -0400

    [WIP] Remove symlink with non-existent shared/

--- peak-blog/src/components/rich-text-editor/DisplayEditor.tsx
+++ peak-blog/src/components/rich-text-editor/DisplayEditor.tsx
@@ -1,10 +1,10 @@
 import React, {useEffect} from "react";
 import { SlatePlugins } from "@udecode/slate-plugins-core";
 import {
+    corePlugins,
     pluginOptions,
     readOnlyProps,
     useReadOnlyComponents,
-    readOnlyPlugins
 } from "component-library/dist";
 
 export interface DisplayEditorProps {
@@ -18,7 +18,7 @@ export const DisplayEditor = ({
     return (
         <SlatePlugins
             id={postId}
-            plugins={readOnlyPlugins}
+            plugins={corePlugins}
             options={pluginOptions}
             components={useReadOnlyComponents()}
             editableProps={readOnlyProps}
