139e843d297500047dd13d564dd11f6555d7cff9
DevonPeroutky <devonperoutky@gmail.com>
2021-05-31 14:20:48 -0700

    [Blog] Adding some more styles

--- peak-blog/src/components/blog/post/post-preview.tsx
+++ peak-blog/src/components/blog/post/post-preview.tsx
@@ -15,14 +15,14 @@ export const BlogPostPreview = (props: { post: PeakPost }) => {
     const title = Node.string(titleNode)
 
     return (
-        <div className={"mb-24"}>
+        <div className={"mb-20"}>
             <Link href={`post/${post.id}`}>
-                <h1 className={"mb-6 cursor-pointer hover:text-blue-500 font-semibold"}>{title}</h1>
+                <h1 className={"mb-2 cursor-pointer hover:text-blue-500 font-semibold text-4xl"}>{title}</h1>
             </Link>
-            <div className={"mb-4 text-lg text-gray-500 font-light"}>
+            <div className={"mb-2 text-lg text-gray-500 font-light"}>
                 {post.subtitle}
             </div>
-            <div className={"text-base text-gray-400 font-light text-sm mb-6"}>
+            <div className={"text-base text-gray-400 font-light text-sm mb-4"}>
                 <span className={"mr-1"}>{author.given_name} {author.family_name}</span> • <span className={"mx-1"}>{moment(post.created_at).format('LL') }</span> • <EstimateReadTime className={"mx-1"} body={post.body}/>
             </div>
         </div>
