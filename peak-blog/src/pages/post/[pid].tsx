cab68818f1b7d194adaaab3f3610de16d77e3725
DevonPeroutky <devonperoutky@gmail.com>
2021-05-31 11:29:01 -0700

    [Blog] cleanup

--- peak-blog/src/pages/post/[pid].tsx
+++ peak-blog/src/pages/post/[pid].tsx
@@ -14,40 +14,30 @@ const Post: NextPage<{}> = (props) => {
     const post_id: string = router.query["pid"] as string
     const queryClient = useQueryClient()
 
-    const fetch_post_wrapper = () => {
-        if (!post_id) {
-            console.log(`The undefined`)
-            return undefined
-        }
-        return fetch_post(post_id)
-    }
-
     const { isLoading, isError, status, data, error } = useQuery<PeakPost, Error>(
         [POST_KEY_PREFIX, post_id],
-        fetch_post_wrapper,
+        () => fetch_post(post_id),
         {
             initialData: () => {
                 if (!post_id) return undefined
-                const posts: PeakPost[] = queryClient.getQueryData<PeakPostListResponse>(POSTS_KEY)?.pages.flatMap(page => page.posts)
+                const posts: PeakPost[] = queryClient.getQueryData<{pages: PeakPostListResponse}>(POSTS_KEY)?.pages.flatMap(page => page.posts)
                 return (posts) ? posts.find(p => p.id === post_id) : undefined
             }
         }
     )
 
-    console.log(`WTFFFF `, status)
-    console.log(`Hello `, isError)
+    console.log(`THE ERROR: `, error)
+    console.log(`Status `, status)
 
     if (isError) {
         console.log(`THe error: `, error)
         return <Error statusCode={500}/>
     }
 
-
     if (isLoading) {
         return <div>Load THIS HO!</div>
     }
 
-    // @ts-ignore
     return (
         <BlogPost post={data}/>
     )
