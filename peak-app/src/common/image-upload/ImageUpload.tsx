53965c7477f555f6111fbb936b0610483fa09352
DevonPeroutky <devonperoutky@gmail.com>
2021-06-12 02:07:11 -0700

    [App] Holly shit we have a file upload that works!

--- peak-app/src/common/image-upload/ImageUpload.tsx
+++ peak-app/src/common/image-upload/ImageUpload.tsx
@@ -4,90 +4,47 @@ import {UploadOutlined} from "@ant-design/icons/lib";
 import {PeakAccessToken, useUploadToken} from "../../client/tokens";
 import {useCurrentUser} from "../../utils/hooks";
 import peakAxiosClient from "../../client/axiosConfig";
-import {sleep} from "../../chrome-extension/utils/generalUtil";
 
 export const ImageUpload = (props: {}) => {
     const currentUser = useCurrentUser()
-    const [contentType, setContentType] = useState<string>("")
     const [accessToken, setAccessToken] = useState<PeakAccessToken>()
-    const [imageDataUrl, setImageData] = useState()
+    const [ loading, setLoading ] = useState<boolean>(false)
     useUploadToken().then(token => setAccessToken(token))
 
     const bucketName = "peak_user_images"
     const baseUrl = "https://storage.googleapis.com/upload/storage/v1/b/"
 
-    function getBinaryString(img, callback) {
-        const reader = new FileReader();
-        console.log(`Reading in....... `, img)
-        reader.addEventListener('load', () => callback(reader.result));
-        reader.readAsBinaryString(img);
-    }
-    function getBase64(img, callback) {
-        const reader = new FileReader();
-        console.log(`Reading in....... `, img)
-        reader.addEventListener('load', () => callback(reader.result));
-        reader.readAsDataURL(img);
-    }
-    function getText(img, callback) {
-        const reader = new FileReader();
-        console.log(`Reading in....... `, img)
-        reader.addEventListener('load', () => callback(reader.result));
-        reader.readAsText(img);
-    }
-
     // `${baseUrl}${bucketName}/o?uploadType=media&name=${userId}/${filename}.${fileType}`
     const uploadProps = (!accessToken) ? { disabled: true } : {
         action: (file) => {
-            // return Promise.resolve(`${baseUrl}${bucketName}/o?uploadType=media&name=${currentUser.id}/${file.name}`)
-            return Promise.resolve(`${baseUrl}${bucketName}/o?uploadType=multipart`)
+            return Promise.resolve(`${baseUrl}${bucketName}/o?uploadType=media&name=${currentUser.id}/${file.name}`)
         },
         headers: {
             'Authorization': `Bearer ${accessToken.token}`
         },
         customRequest: (fileWrapper) => {
             const file = fileWrapper.file
-            peakAxiosClient
-                .post(
-                    `${baseUrl}${bucketName}/o?uploadType=media&name=${currentUser.id}/${file.name}`,
-                   file,
-                    {
-                        headers: {
-                            'content-type': file.type,
-                            'Authorization': `Bearer ${accessToken.token}`
-                        }
-                    })
-            // getBase64(file, imageUrl => {
-            //     console.log(`Base64: `, imageUrl)
-            //     const stripped = imageUrl.substring(23)
-            //     console.log(`Base64 - stripped: `, stripped)
-            //     peakAxiosClient
-            //         .post(
-            //             `${baseUrl}${bucketName}/o?uploadType=media&name=${currentUser.id}/${file.name}`,
-            //             {
-            //                 data: stripped
-            //             },
-            //             {
-            //                 headers: {
-            //                     'content-type': file.type,
-            //                     'Authorization': `Bearer ${accessToken.token}`
-            //                 }
-            //             })
-            // })
-            // getBinaryString(file, imageUrl => {
-            //     console.log(`BitchyBoyyyy - BINARY: `, imageUrl)
-            //     peakAxiosClient
-            //         .post(
-            //             `${baseUrl}${bucketName}/o?uploadType=media&name=${currentUser.id}/binary-${file.name}`,
-            //             {
-            //                 data: imageUrl
-            //             },
-            //             {
-            //                 headers: {
-            //                     'content-type': file.type,
-            //                     'Authorization': `Bearer ${accessToken.token}`
-            //                 }
-            //             })
-            // })
+            setLoading(true)
+            peakAxiosClient.post(fileWrapper.action, file, {
+                headers: {
+                    'content-type': file.type,
+                    'Authorization': `Bearer ${accessToken.token}`
+                }
+            }).catch(_ => {
+                notification.error({message: `File upload failed`});
+            }).finally(() => setLoading(false))
+        },
+        onChange(info) {
+            console.log(`CHANGING `, info)
+            if (info.file.status !== 'uploading') {
+            }
+            if (info.file.status === 'done') {
+                notification.success({message: `File uploaded successfully`});
+                setLoading(false)
+            } else if (info.file.status === 'error') {
+                notification.error({message: `File upload failed`});
+                setLoading(false)
+            }
         },
     };
 
@@ -96,7 +53,7 @@ export const ImageUpload = (props: {}) => {
 
     return (
         <Upload {...uploadProps}>
-            <Button icon={<UploadOutlined />}>Click to Upload</Button>
+            <Button icon={<UploadOutlined />} loading={loading}>Click to Upload</Button>
         </Upload>
     )
 }
